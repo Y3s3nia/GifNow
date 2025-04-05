410717ea9557c073ad4a3218fac80a24410b24d8
Y3s3nia <y3s3nian3ri@gmail.com>
2025-04-05 17:02:10 -0500

    Added to the scrpit and html page.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let GifContainer = document.querySelector('.js-gif-container');
 let StickerContainer = document.querySelector('.js-sticker-container');
+let search = document.querySelector('.searchBTN');
+
+search.addEventListener('click', async () => {
+    const searchValue = search.value;
+    const gifData = await fetchGif(searchValue);
+    const stickerData = await fetchSticker(searchValue);
+    displayGif(gifData);
+    displaySticker(stickerData);
+});
 
 async function fetchGif() {
-    const data = await fetch(`https://api.giphy.com/v1/stickers/search?api_key=JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa&q=${search}&limit=25&offset=0`);
+    const data = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa&q=${search}`);
     const response = await data.json();
     categories = response.categories;
     return {
