e13a7bfa944b8c007a61dff209a6aace5d84d0be
Y3s3nia <y3s3nian3ri@gmail.com>
2025-04-05 00:28:11 -0500

    Added the searchbar and added some js.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let GifContainer = document.querySelector('.js-gif-container');
 let StickerContainer = document.querySelector('.js-sticker-container');
+
+async function fetchGif() {
+    const data = await fetch(`https://api.giphy.com/v1/stickers/search?api_key=JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa&q=${search}&limit=25&offset=0`);
+    const response = await data.json();
+    categories = response.categories;
+    return {
+        
+    };
+}
