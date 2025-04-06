809ddd3817af5f0ffae26dc185027ba83c79fd1b
Y3s3nia <y3s3nian3ri@gmail.com>
2025-04-05 22:32:25 -0500

    fixed the api and is now showing up.

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 let GifContainer = document.querySelector('.js-gif-container');
 let StickerContainer = document.querySelector('.js-sticker-container');
 let search = document.querySelector('.searchBTN');
+let searchInput = document.querySelector('.search-input');
 
-search.addEventListener('click', async () => {
-    const searchValue = search.value;
-    const gifData = await fetchGif(searchValue);
-    const stickerData = await fetchSticker(searchValue);
-    displayGif(gifData);
-    displaySticker(stickerData);
-});
+search.addEventListener('click', fetchGif);
+search.addEventListener('click', fetchSticker);
 
 async function fetchGif() {
-    const data = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa&q=${search}`);
+    const keyAPI = 'JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa';
+    const URL= `https://api.giphy.com/v1/gifs/search?api_key=${keyAPI}&q=${searchInput.value}&limit=15`;
+    const data = await fetch(URL);
     const response = await data.json();
-    categories = response.categories;
-    return {
-        
-    };
+    displayGif(response.data);
+    return response.data;
+}
+
+async function fetchSticker() {
+    const keyAPI = 'JqmvYQRoKo3mKEnXit47ikgZRqWVDXVa';
+    const URL= `https://api.giphy.com/v1/stickers/search?api_key=${keyAPI}&q=${searchInput.value}&limit=15`;
+    const data = await fetch(URL);
+    const response = await data.json();
+    displaySticker(response.data);
+    return response.data;
+}
+
+function renderGif(data) {
+    return `
+            <img class="gif-item"
+                src="${data.images.original.url}" 
+                alt="${data.title}" />
+    `;
+}
+
+function renderSticker(data) {
+    return `
+            <img class="sticker-item"
+                src="${data.images.original.url}" 
+                alt="${data.title}" />
+    `;
+}
+
+function displayGif(gifData) {
+    GifContainer.innerHTML = gifData
+        .map(gif => renderGif(gif))
+        .join('');
+}
+
+function displaySticker(stickerData) {
+    StickerContainer.innerHTML = stickerData
+        .map(sticker => renderSticker(sticker))
+        .join('');
 }
